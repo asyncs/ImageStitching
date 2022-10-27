d6a00105460da6983560e05604e9945cff17e2bf
Arda Sarp Yenicesu <ardasarpyenicesu@gmail.com>
2022-10-27 19:20:45 +0300

    -SIFT and Feature Matching Done -Feature matching transpose error

---
+++ ImageStitcher.py
@@ -0,0 +1,83 @@
+import cv2 as cv
+import numpy as np
+from scipy.spatial import distance
+from skimage.feature import plot_matches
+import matplotlib.pyplot as plt
+
+
+class ImageStitcher:
+
+    def __init__(self, image_array):
+        self.images = image_array
+        self.keypoints_array = []
+        self.descriptors_array = []
+        self.matches = []
+
+    def feature_extraction(self):
+        tmp_images = self.images
+        sift_keypoints_array = []
+        sift_descriptors_array = []
+
+        sift = cv.SIFT_create()
+
+        for img in tmp_images:
+            #img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            keypoints, descriptors = sift.detectAndCompute(img, None)
+            sift_keypoints_array.append(keypoints)
+            sift_descriptors_array.append(descriptors)
+
+            img_keypoint = cv.drawKeypoints(img, keypoints, None)
+            plt.imshow(cv.cvtColor(img_keypoint, cv.COLOR_BGR2RGB))
+            plt.show()
+
+        self.keypoints_array = sift_keypoints_array
+        self.descriptors_array = sift_descriptors_array
+
+    #Sorting logic is inspired from https://github.com/AhmedHisham1/ORB-feature-matching/blob/master/utils.py
+    def feature_matching_brute(self):
+        matches = []
+        for pair_index in range(len(self.descriptors_array) - 1):
+            image_a = self.images[pair_index]
+            image_b = self.images[pair_index + 1]
+            keypoints_a = self.keypoints_array[pair_index]
+            keypoints_b = self.keypoints_array[pair_index + 1]
+            descriptor_a = self.descriptors_array[pair_index]
+            descriptor_b = self.descriptors_array[pair_index + 1]
+
+            distances_a2b = distance.cdist(descriptor_a, descriptor_b, metric='hamming')
+
+            index_a = np.arange(descriptor_a.shape[0])
+            index_b = np.argmin(distances_a2b, axis=1)
+
+            matches_b2a = np.argmin(distances_a2b, axis=0)
+            cross_check = index_a == matches_b2a[index_b]  # len(mask) = len(d1)
+
+            index_a = index_a[cross_check]
+            index_b = index_b[cross_check]
+            distances_a2b = distances_a2b[index_a, index_b]
+            sorted_indexes = distances_a2b.argsort()
+
+            match = np.column_stack((index_a[sorted_indexes], index_b[sorted_indexes]))
+            matches.append(match)
+
+            keypoints_a_loc = np.flip(cv.KeyPoint_convert(keypoints_a), 1)
+            keypoints_b_loc = np.flip(cv.KeyPoint_convert(keypoints_b), 1)
+            print(keypoints_a_loc.shape)
+            fig = plt.figure(figsize=(20, 10))
+            ax = fig.add_subplot(1, 1, 1)
+
+            plot_matches(ax, cv.cvtColor(image_a, cv.COLOR_BGR2RGB), cv.cvtColor(image_b, cv.COLOR_BGR2RGB),
+                         cv.KeyPoint_convert(keypoints_a).T, cv.KeyPoint_convert(keypoints_b), match[:20])
+            plt.show()
+
+        self.matches = matches
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+import os
+import cv2 as cv
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'sarp, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from ImageStitcher import ImageStitcher
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    img_path = 'data_image_stitching/'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    images = []
+    sub_images = []
+    prev_image_index = '1'
+
+    for image in os.listdir(img_path):
+        image_name = os.fsdecode(image)
+        if prev_image_index != image_name[2]:
+            images.append(sub_images)
+            sub_images = []
+            prev_image_index = image_name[2]
+            print("Moving to image no: ", prev_image_index)
+
+        img = cv.imread(img_path + image_name)
+        sub_images.append(img)
+
+    sub_images.append(img)
+
+    stitcher1 = ImageStitcher(images[0])
+    stitcher1.feature_extraction()
+    stitcher1.feature_matching_brute()
+
+
